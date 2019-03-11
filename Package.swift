3053eff92500963ffe784c150a49a0039fc3df02
Max Liberman <max.y.liberman@gmail.com>
2019-03-11 17:18:18 -0400

    Move All Dependencies to Local System Libraries

--- Package.swift
+++ Package.swift
@@ -4,15 +4,52 @@ import PackageDescription
 let package = Package(
     name: "Cairo",
     products: [
+        .library(name: "CCairoJPEG", targets: ["CCairoJPEG"]),
         .library(name: "Cairo", targets: ["Cairo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mliberman/CCairo.git", from: "1.2.0"),
-        .package(url: "https://github.com/mliberman/CCairoJPEG.git", from: "1.0.4"),
-        .package(url: "https://github.com/mliberman/CFontConfig.git", from: "1.1.0"),
-        .package(url: "https://github.com/mliberman/CFreeType.git", from: "1.1.0"),
     ],
     targets: [
+        .systemLibrary(
+            name: "CCairo",
+            pkgConfig: "cairo",
+            providers: [
+                .apt(["libcairo-dev"]),
+                .brew(["cairo"]),
+            ]
+        ),
+        .systemLibrary(
+            name: "CFontConfig",
+            pkgConfig: "fontconfig",
+            providers: [
+                .brew(["fontconfig"]),
+                .apt(["libfontconfig-dev"])
+            ]
+        ),
+        .systemLibrary(
+            name: "CFreeType",
+            pkgConfig: "freetype2",
+            providers: [
+                .apt(["libfreetype6-dev"]),
+                .brew(["freetype2"])
+            ]
+        ),
+        .systemLibrary(
+            name: "Cjpeg",
+            pkgConfig: "libjpeg",
+            providers: [
+                .brew(["libjpeg"]),
+                .apt(["libjpeg-dev"])
+            ]
+        ),
+        .target(
+            name: "CCairoJPEG",
+            dependencies: [
+                "CCairo",
+                "Cjpeg"
+            ],
+            path: "Sources/CCairoJPEG"
+        ),
         .target(
             name: "Cairo",
             dependencies: [
